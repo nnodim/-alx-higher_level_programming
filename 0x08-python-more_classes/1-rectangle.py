2ecf63f75339f08c0b5d730f4f1973acdf79c9e6
nnodim <nnodimobinna@gmail.com>
2022-09-20 01:11:39 +0100

    1. Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,47 +1,58 @@
-
 #!/usr/bin/python3
-"""A class that defines a rectangle"""
+"""Module 1-rectangle
+This Module contains an definition for Rectangle class
+"""
 
 
 class Rectangle:
-    """this represents a rectangle"""
+    """A class to represent a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Initializing this rectangle class
+        """initializes a rectangle with width and height
         Args:
-            width: represents the width of the rectangle
-            height: represents the height of the rectangle
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is less than zero
+            width (int, optional): width of the rect. Defaults to 0.
+            height (int, optional): height of the rect. Defaults to 0.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """retrieves width attribute"""
+        """width property"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """sets width attribute"""
-        if not isinstance(value, int):
+        """width property setter
+        Args:
+            value (int): value
+        Raises:
+            TypeError: if width is not an int
+            ValueError: if width is negative
+        """
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """retrieves height attribute"""
+        """height property"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """sets height attribute"""
-        if not isinstance(value, int):
+        """height property setter
+        Args:
+            value (int): value
+        Raises:
+            TypeError: if height is not an int
+            ValueError: if height is negative
+        """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
