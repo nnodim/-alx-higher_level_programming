ec9be49831c61861a3f5da5cf3ebf59201e9328c
nnodim <nnodimobinna@gmail.com>
2022-12-29 04:43:08 -0800

    6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
-"""
-import sys
-import requests
-
+""" A POST request to the passed URL
+with the email as a parameter,
+and finally displays the body of the response."""
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    import sys
+    import requests
 
-    r = requests.post(url, data=value)
-    print(r.text))
+    response = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(response.text)
